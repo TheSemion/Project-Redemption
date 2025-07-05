8c4bea0371262f5935da0d444f1f22bf3c05fa74
TheSemion <ostapsemionyk1403@gmail.com>
2025-07-05 14:20:04 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket("ws://localhost:3000")
+const socket = new WebSocket("wss://project-name.up.railway.app")
 
 window.addEventListener("DOMContentLoaded", () => {
     const clientNumber = localStorage.getItem("currentClient")
